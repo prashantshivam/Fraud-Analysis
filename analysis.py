d631572dfcec61146754f71edc3cdca7965a9cf7
prashantshivam <prashant.kumar@iiitdmj.ac.in>
2018-02-27 11:44:12 +0530

    first assignment modified

--- analysis.py
+++ analysis.py
@@ -17,6 +17,7 @@ df=pd.concat(df4)
 #finding all references to Donald Trump
 references=df[df['text'].str.contains("@realDonaldTrump")]
 #printing all refernces
+print("The account referncing donald trump are:\n")
 print(references)
 
 #finding total number of accounts
@@ -27,13 +28,13 @@ donaldtrump_related_accounts=references.groupby('user_id').size().shape
 #findin total percentage of accounts tweeting about Donald Trump
 total_percentage=(float(donaldtrump_related_accounts[0])/float(total_accounts[0]))*100
 #printing total percentage
-test=total_percentage.tostring()
-print("The total % of account tweeting about donald trump is ")
-print(total_percentage)
+test=str(total_percentage)
+print("The total % of account tweeting about donald trump is "+test)
 
 #counting number of tweets referencing Donald Trump from each user accounts
 frequency=references.groupby(['user_id','screen_name'])['text'].count()
 #sorting user'id based on the count in descending order
 accounts_in_descending_order=frequency.nlargest(donaldtrump_related_accounts[0])
 #printng the accounts
+print("The accounts in descending order of frequency:")
 print(accounts_in_descending_order)
